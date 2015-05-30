530fc9be1f8f015b6afd4c081a7d8eb764e05cec
Son Nguyen Kim <nguyenkims@users.noreply.github.com>
2015-05-30 11:15:10 +0200

    add startDate, endDate, clearDate(), getDates() to controller

--- static/client.js
+++ static/client.js
@@ -1,4 +1,4 @@
-var app = angular.module('joggingApp', []);
+var app = angular.module('joggingApp', ['ui.bootstrap']);
 
 app.controller('mainCtrl', function ($scope, $http) {
     console.log("mainCtrl");
@@ -9,4 +9,34 @@ app.controller('mainCtrl', function ($scope, $http) {
         {date: Date.now(), distance: 10.2, time: 20.3},
         {date: new Date(1995, 11, 17), distance: 7.2, time: 2.3}
     ];
+
+    $scope.startDate = null;
+    $scope.endDate = null;
+
+    $scope.clearDate = function(){
+        $scope.startDate = null;
+        $scope.endDate = null;
+    };
+
+    $scope.getDates = function(){
+        startDate = $scope.startDate;
+        endDate = $scope.endDate;
+
+        console.log("startDate:" + startDate);
+        res = [];
+
+        for (i =0; i < $scope.entries.length; i++){
+            d = $scope.entries[i].date;
+            startOk = (startDate != null && startDate < d) || (startDate == null);
+            endOk = (endDate != null && endDate > d) || (endDate == null);
+
+            if (startOk && endOk)
+                res.push($scope.entries[i]);
+            else
+                console.log("do not push:" + d);
+        }
+
+        return res;
+    }
+
 });
